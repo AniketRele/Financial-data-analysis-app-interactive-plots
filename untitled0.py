e8bc711fec8c1d02650f465a2f68da1f25e5a6a1
Aniket Rele <55667441+AniketRele@users.noreply.github.com>
2021-04-25 18:14:21 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 16:25:26 2021
+
+@author: aniket.rele
+"""
+
+import streamlit as st
+import pandas as pd
+import plotly.figure_factory as ff
+from chart_studio import plotly
+import plotly.express as px
+
+
+@st.cache
+def load_data():
+    data=pd.read_csv(r"C:\Users\Aniket.Rele\Desktop\upwork\data\data\Output.csv")
+    return data
+
+st. set_page_config(layout="wide")
+st.title('US Bitcoin Market Analysis- Bitcoin, Litcoin & Etherium')
+#st.title('-------------------------------------------------------------------------------')
+fig_open = px.area(load_data(),x='Date',y=['Open_Bitcoin','Open_Litcoin','Open_Etherium','Open_Gold','Open_dollar','Open_sap500'],
+                  labels={'Date':'Date', 'value':'Open'}, height=700)
+fig_open.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+
+fig_close = px.area(load_data(),x='Date',y=['Close_Bitcoin','Close_Litcoin','Close_Etherium','Close_Gold','Close_dollar','Close_sap500'],
+                   labels={'Date':'Date', 'value':'Close'}, height=700)
+fig_close.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+
+fig_low = px.area(load_data(),x='Date',y=['Low_Bitcoin','Low_Litcoin','Low_Etherium','Low_Gold','Low_dollar','Low_sap500'],
+                 labels={'Date':'Date', 'value':'Low'}, height=700)
+fig_low.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+
+fig_high = px.area(load_data(),x='Date',y=['High_Bitcoin','High_Litcoin','High_Etherium','High_Gold','High_dollar','High_sap500'],
+                  labels={'Date':'Date', 'value':'High'}, height=700)
+fig_high.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+
+fig_ac = px.area(load_data(),x='Date',y=['Adj Close_Bitcoin','Adj Close_Litcoin','Adj Close_Etherium','Adj Close_Gold','Adj Close_dollar','Adj Close_sap500'],
+                labels={'Date':'Date', 'value':'Adjacent Close'}, height=700)
+fig_ac.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+
+fig_vol = px.area(load_data(),x='Date',y=['Volume_Bitcoin','Volume_Litcoin','Volume_Etherium','Volume_Gold','Volume_dollar','Volume_sap500'],
+                 labels={'Date':'Date', 'value':'Volume'}, height=700)
+fig_vol.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+st.subheader('Comparison of Open for the currencies')
+st.plotly_chart(fig_open, use_container_width=True)
+st.subheader('Comparison of Close for the currencies')
+st.plotly_chart(fig_close, use_container_width=True)
+st.subheader('Comparison of Low for the currencies')
+st.plotly_chart(fig_low, use_container_width=True)
+st.subheader('Comparison of High for the currencies')
+st.plotly_chart(fig_high, use_container_width=True)
+st.subheader('Comparison of Adjacent Close for the currencies')
+st.plotly_chart(fig_ac, use_container_width=True)
+st.subheader('Comparison of Volume for the currencies')
+st.plotly_chart(fig_vol, use_container_width=True)
